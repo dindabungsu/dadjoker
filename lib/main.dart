21a22f197d787ebe55520f17e042e2a4f74de790
Dinda Ragil Pramuda Wardani <dindapramuda@gmail.com>
2026-01-07 14:41:49 +0700

    feat: remove unnecessary packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/adapters.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
   final container = ProviderContainer();
   runApp(
     UncontrolledProviderScope(
